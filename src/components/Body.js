c626f907097328f79c2b344dceb38ad9dd31a94b
Ch V Ajay Reddy <ajayreddy1997@gmail.com>
2025-09-08 21:40:36 +0530

    Features Added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,46 +4,79 @@ import { LuBellRing } from "react-icons/lu";
 import { MdAccountCircle } from "react-icons/md";
 import { SlRefresh } from "react-icons/sl";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 const Body = () => {
-  const [category, setcategory] = useState("");
+  const [form, setform] = useState({
+    categoryName: "",
+    widgetName: "",
+    widgetText: "",
+  });
+  const [searchInput, setsearchInput] = useState("");
   const [display, setdisplay] = useState(data);
-  const [wid, setwid] = useState("");
-  const [cate, setcate] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [originalData, setoriginalData] = useState(data);
 
-  function categoryadd(cat) {
-    let a = {};
-    a.categoryName = cat;
-    setdisplay((prevdisplay) => [...prevdisplay, a]);
-  }
-  function addwidget() {
-    let foundcate = display.filter((dis) => dis.categoryName === cate);
-    if (foundcate[0].widgtes == null) {
-      foundcate[0].widgtes = [];
-      foundcate[0].widgtes.push(wid);
-    } else {
-      foundcate[0].widgtes.push(wid);
-    }
-    let repl = foundcate[0];
-
-    setdisplay((prevdisplay) =>
-      prevdisplay.map((user) =>
-        user.categoryName === cate ? { ...user, repl } : user
+  function addWidget() {
+    let tempCategory = originalData.filter(
+      (category) => category.categoryName === form.categoryName
+    );
+    console.log(tempCategory[0]);
+    let tempWidget = {};
+    tempWidget.widgetName = form.widgetName;
+    tempWidget.widgetText = form.widgetText;
+    tempCategory[0].widgets.push(tempWidget);
+    console.log(tempCategory);
+    let updatedCategory = tempCategory[0];
+    setoriginalData((prevoriginalData) =>
+      prevoriginalData.map((category) =>
+        category.categoryName === form.categoryName
+          ? { ...category, updatedCategory }
+          : category
       )
     );
+    setdisplay(originalData);
   }
-  function fndelete(category, index) {
-    let foundcate = display.filter((dis) => dis.categoryName === category);
-    foundcate[0].widgtes.splice(index, 1);
+  function deleteWidget(categoryName, index) {
+    let tempCategory = originalData.filter(
+      (category) => category.categoryName === categoryName
+    );
 
-    let repl = foundcate[0];
-
-    setdisplay((prevdisplay) =>
-      prevdisplay.map((user) =>
-        user.categoryName === category ? { ...user, repl } : user
+    tempCategory[0].widgets.splice(index, 1);
+    let updatedCategory = tempCategory[0];
+    setoriginalData((prevoriginalData) =>
+      prevoriginalData.map((category) =>
+        category.categoryName === form.categoryName
+          ? { ...category, updatedCategory }
+          : category
       )
     );
+    setdisplay(originalData);
+  }
+  function search(e) {
+    let searchValue = e.target.value.toLowerCase();
+    setsearchInput(searchValue);
+    let searchOutput = originalData
+      .map((category) => {
+        if (category.categoryName.toLowerCase().includes(searchValue)) {
+          return category;
+        }
+
+        let matchedWidgets =category.widgets.filter((widget) =>
+          widget.widgetName.toLowerCase().includes(searchValue)
+        );
+        if (matchedWidgets.length > 0) {
+          return { ...category, widgets: matchedWidgets };
+        }
+        return null;
+      })
+      .filter(Boolean);
+    setdisplay(searchOutput);
   }
 
+  const formIsValid =
+    form.widgetName !== "" && form.categoryName !== "Select Category";
   return (
     <div id="main">
       <div id="header" style={{ display: "flex", flexDirection: "row" }}>
@@ -66,6 +99,8 @@ const Body = () => {
             type="text"
             style={{ width: "40%" }}
             placeholder="Search Anything"
+            value={searchInput}
+            onChange={(e) => search(e)}
           ></input>
           <LuBellRing />
           <p>
@@ -83,7 +118,13 @@ const Body = () => {
           </div>
 
           <div id="dashboardIcons">
-            <button>Add Widget +</button>
+            <button
+              onClick={() => {
+                setModalIsOpen(true);
+              }}
+            >
+              Add Widget +
+            </button>
             <button>
               <SlRefresh />
             </button>
@@ -93,54 +134,109 @@ const Body = () => {
           </div>
         </div>
         <div id="dashboardLayout">
-        {display.map((d, index) => (
-          <div key={index}>
-            <div>
-              <p ><strong>{d.categoryName}</strong> </p>
-              <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "33%  33% 33%",
-                  gap: 12,
-                }}
-              >
-                {d.widgtes.map((wid, index) => (
-                      <div
-                        className="widget"
-                        key={index}
-                        style={{ height: "auto" ,backgroundColor:"white",borderRadius:"20px",padding:"10px" }}
+          {display.map((category, index) => (
+            <div key={index}>
+              <div>
+                <p>
+                  <strong>{category.categoryName}</strong>
+                </p>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "33%  33% 33%",
+                    gap: 12,
+                  }}
+                >
+                  {category.widgets.map((widget, index) => (
+                    <div className="widget" key={index}>
+                      <button
+                        className="removebtn"
+                        onClick={() =>
+                          deleteWidget(category.categoryName, index)
+                        }
                       >
-                        {" "}
-                        <p style={{margin:"0px",fontSize:"14px",maxHeight:"auto"}}>{wid}</p>
-
-                        <img style={{padding:"10px",maxWidth:"100%",maxHeight:"auto", display: "block",objectFit:"cover"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrvZwiS8mHVedATgm-Mhmj9dHzuXBgzoVFA&s"></img>
-                        
-                        
-                      </div>
-                    ))}
+                        <RxCross2 color="red" />
+                      </button>{" "}
+                      <p
+                        style={{
+                          margin: "0px",
+                          fontSize: "14px",
+                          maxHeight: "auto",
+                        }}
+                      >
+                        {widget.widgetName}
+                      </p>
+                      <p> {widget.widgetText}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}</div>
-                <input
-          type="text"
-          value={category}
-          onChange={(e) => setcategory(e.target.value)}
-        ></input>
-        <button onClick={() => categoryadd(category)}>Add category</button>
-
-        <select id="fruits" onChange={(e) => setcate(e.target.value)}>
-          {display.map((dis, index) => (
-            <option key={index}>{dis.categoryName}</option>
           ))}
-        </select>
-        <input
-          type="text"
-          onChange={(e) => setwid(e.target.value)}
-          value={wid}
-        ></input>
-        <input onClick={() => addwidget()} type="submit" value="Submit" />
+        </div>
       </div>
+      <Modal isOpen={modalIsOpen}>
+        <div>
+          {" "}
+          <RxCross2
+            id="close-btn"
+            onClick={() => setModalIsOpen(false)}
+            style={{
+              width: "20px",
+              height: "20px",
+              color: "red",
+              cursor: "pointer",
+              border: "1px solid red",
+              borderRadius: "10px",
+            }}
+          />
+        </div>
+        <div
+          id="modalBody"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            alignContent: "center",
+          }}
+        >
+          <label htmlFor="formCategory">Select Category:</label>
+          <select
+            id="formCategory"
+            value={form.categoryName}
+            onChange={(e) => setform({ ...form, categoryName: e.target.value })}
+          >
+            <option>Select Category</option>
+            {display.map((category, index) => (
+              <option key={index}>{category.categoryName}</option>
+            ))}
+          </select>
+          <label htmlFor="formWidget">Widget Name :</label>
+          <input
+            type="text"
+            id="formWidget"
+            value={form.widgetName}
+            onChange={(e) => setform({ ...form, widgetName: e.target.value })}
+          ></input>
+          <label htmlFor="formWidgetText">Widget text :</label>
+          <input
+            type="text"
+            id="formWidgetText"
+            value={form.widgetText}
+            onChange={(e) => setform({ ...form, widgetText: e.target.value })}
+          ></input>
+
+          <button
+            disabled={!formIsValid}
+            onClick={() => {
+              addWidget();
+              setModalIsOpen(false);
+            }}
+          >
+            Add Widget
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
